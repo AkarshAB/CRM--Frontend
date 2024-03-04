48b167ecb5f878a7283548cc67b4d7f565d5a3cd
aravindwilson <aravindwilson000@gmail.com>
2024-03-04 15:00:39 +0530

    service complite

--- src/components/ServiceRequests/ServiceRequests.jsx
+++ src/components/ServiceRequests/ServiceRequests.jsx
@@ -1,8 +1,70 @@
 import React from 'react'
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Button from '@mui/material/Button';
+import { GridExpandMoreIcon } from '@mui/x-data-grid';
+
 
 function ServiceRequests() {
   return (
-    <div>ServiceRequests</div>
+    <>
+    <h1 className='text-center'>Service Requests</h1>
+    <div className='container w-75'>
+    <Accordion className='mb-1'>
+        <AccordionSummary
+          expandIcon={<GridExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          Accordion Actions
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+        <AccordionActions>
+          <Button><i className="fa-solid fa-xl fa-xmark"></i></Button>
+          <Button><i className="fa-solid fa-xl fa-check"></i></Button>
+        </AccordionActions>
+      </Accordion>
+    <Accordion className='mb-1'>
+        <AccordionSummary
+          expandIcon={<GridExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          Accordion Actions
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+        <AccordionActions>
+          <Button><i className="fa-solid fa-xl fa-xmark"></i></Button>
+          <Button><i className="fa-solid fa-xl fa-check"></i></Button>
+        </AccordionActions>
+      </Accordion>
+    <Accordion className='mb-1'>
+        <AccordionSummary
+          expandIcon={<GridExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          Accordion Actions
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+        <AccordionActions>
+          <Button><i className="fa-solid fa-xl fa-xmark"></i></Button>
+          <Button><i className="fa-solid fa-xl fa-check"></i></Button>
+        </AccordionActions>
+      </Accordion>
+    </div>
+     </>   
   )
 }
 
