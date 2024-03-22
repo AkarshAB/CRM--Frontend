2864e857d6679df3da05241d34465eaf987dba84
AkarshBharathan <akarshab361@gmail.com>
2024-03-22 16:48:57 +0530

    stocks view page completed

--- src/components/Stocks/Stocks.jsx
+++ src/components/Stocks/Stocks.jsx
@@ -36,51 +36,41 @@ function Stocks() {
 
   const columns = [
     { field: 'id', headerName: 'Prodct ID', width: 230 },
-    { field: 'productName', headerName: 'Product Name', width: 230 },
-    { field: 'category', headerName: 'Category', width: 230 },
+    { field: 'pro_company', headerName: 'Product Company', width: 230 },
+    { field: 'productname', headerName: 'Product Name', width: 230 },
     {
-      field: 'StockAvailable', headerName: 'Stock Available', width: 230, editable: true
+      field: 'quantity', headerName: 'Stock Available', width: 230,
     },
-    {
-      field: 'Price', headerName: 'Price', width: 230,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-  ];
-
-  const rows = [
-    { id: 1, productName: 'Snow', category: 'Jon', StockAvailable: 35 },
-    { id: 2, productName: 'Lannister', category: 'Cersei', StockAvailable: 42 },
-    { id: 3, productName: 'Lannister', category: 'Jaime', StockAvailable: 45 },
-    { id: 4, productName: 'Stark', category: 'Arya', StockAvailable: 16 },
-    { id: 5, productName: 'Targaryen', category: 'Daenerys', StockAvailable: 45 },
-    { id: 6, productName: 'Melisandre', category: null, StockAvailable: 150 },
-    { id: 7, productName: 'Clifford', category: 'Ferrara', StockAvailable: 44 },
-    { id: 8, productName: 'Frances', category: 'Rossini', StockAvailable: 36 },
-    { id: 9, productName: 'Roxie', category: 'Harvey', StockAvailable: 65 },
+    // {
+    //   field: 'Price', headerName: 'Price', width: 230,
+    //   valueGetter: (params) =>
+    //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    // },
   ];
 
 
   return (
     <>
       <div>
-        <Link to={'/add-stock'}><button className='btn btn-primary ms-5 my-3'>Add Stock</button></Link>
+        <Link to={'/add-stock'}><button className='btn btn-primary ms-5 mt-3'>Add Stock</button></Link>
       </div>
-      <div style={{ height: '61vh', width: '' }} className='my-auto mx-5 d-flex justify-content-center mt-3 '>
-        <DataGrid className='border '
-          rows={rows}
-          columns={columns}
-          autoHeight
-          disableRowSelectionOnClick
-          // onEditCellChange={handleEditCellChange}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
-          pageSizeOptions={[5, 10, 20]}
+      <div className="table-wrapper">
+        <div style={{ height: '61vh', width: '' }} className='my-auto mx-5 d-flex justify-content-center mt-3 table-wrapper'>
+          <DataGrid className='border '
+            rows={stocksData}
+            columns={columns}
+            autoHeight
+            disableRowSelectionOnClick
+            // onEditCellChange={handleEditCellChange}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5, 10, 20]}
 
-        />
+          />
+        </div>
       </div>
     </>
   )
